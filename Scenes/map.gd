f7ffc36044dbd668131e6604df84ab705645f476
box-shaped <boxy.shaped@gmail.com>
2024-09-14 14:11:03 +0100

    Building implemented building, kinda janky and only one blocktype currently

--- Scenes/map.gd
+++ Scenes/map.gd
@@ -2,7 +2,6 @@ extends Node3D
 
 #For 2D games this will be the hight/width of your hexagon sprite
 @export var _cellSize : Vector2 = Vector2(1.15,1.15)
-@export var _mapSize : Vector2 = Vector2(5,10)
 @export var mapDiameter:int=101
 @export var BUMP:float=0.03
 @export var mapHeight = 10
@@ -41,7 +40,6 @@ func initmap(diameter):
 			else:
 				world_map[str(Vector2i(x,y))]=true
 	return(world_map)
-
 func generateMap():
 	var TileLibrary = preload("res://Library.tscn")
 	for x in range(mapDiameter):
@@ -63,8 +61,40 @@ func generateMap():
 				hexTile.get_parent().remove_child(hexTile)
 				hexTile.set_owner(null)
 				add_child(hexTile)
-				
+func placeBlock(blockID:String, coordinates:Vector3):
+	var co0ordinates = Vector2(coordinates.x, coordinates.z)
+	var TileLibrary = preload("res://Library.tscn")
+	var hexTile = TileLibrary.instantiate().find_child(str(blockID))
+	var pixelCoords = pixel_to_pointy_hex(co0ordinates)
+	var center = _cellToPixel(Vector2((mapDiameter-1)/2,(mapDiameter-1)/2))
+	#translate(Vector3(-center[0].x, 0 ,-center[0].y))
+	pixelCoords = _cellToPixel(pixelCoords)
+	print(pixelCoords[1]+1)
+	var tileCoords = Vector3(pixelCoords[0].x+center[0].x,coordinates.y, pixelCoords[0].y+center[0].y)
+	hexTile.position = tileCoords
+	print(tileCoords," SHOULD BE EQUAL TO VALUE BELOW")
+	hexTile.get_parent().remove_child(hexTile)
+	hexTile.set_owner(null)
+	add_child(hexTile)
+	print("BlockPlaced")
+	print(hexTile.position)
 	
+func pixel_to_pointy_hex(point: Vector2) -> Vector2i:
+	# Reverse the calculation done in cellToPixel
+	var x = (point.x * sqrt(3) / 3 - point.y / 3) / _cellSize.x
+	var y = (2 * point.y / 3) / _cellSize.y
+	print("BlockPlaced")
+	return Vector2i(round(x), round(y))
+	
+@onready var ray = $"../Player/SpringArmPivot/SpringArm3D/Camera3D/Ray"
+func _input(event):
+	if event.is_action_pressed("Build"):
+		if ray.is_colliding():
+			print(ray.get_collision_normal())
+			if ray.get_collision_normal().y==1:
+				placeBlock("House1",ray.get_collision_point())
+				print("raycollision",ray.get_collision_point())
+				print("playerpos: ",$"../Player".position)
 
 #code from https://www.redblobgames.com/grids/hexagons/
 #var axial_direction_vectors = [
